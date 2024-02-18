27582e77008e02d85abd6020c0b56780b06040e4
culplate <culplate@gmail.com>
2024-02-18 21:45:14 +0100

    added api requests (trending movies)

---
+++ api.js
@@ -0,0 +1,19 @@
+import axios from 'axios'
+
+const URL = 'https://api.themoviedb.org/3/';
+const searchParam = 'search/movie';
+const trendingParam = 'trending/movie/week?include_adult=true&language=en-US&page=1';
+
+const API_KEY = 'a5e5694b5c6ed25f9fc27a46f040f3b8';
+const API_TOKEN = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhNWU1Njk0YjVjNmVkMjVmOWZjMjdhNDZmMDQwZjNiOCIsInN1YiI6IjY1ZDEwYmNhNjY3NTFkMDE2MzMwZjBlYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.c8h12A5dXK4Ny8FGc_DKQnprEYQ-Rn819Y9Zg3Ffk_Y';
+
+const options = {
+    headers: {
+        Authorization: `Bearer ${API_TOKEN}`
+    }
+}
+export const fetchTrending = async () => {
+    const response = await axios.get(`${URL + trendingParam}`, options)
+    console.log(response.data.results)
+    return response.data.results;
+}
--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,7 +1,33 @@
+import { useEffect, useState } from "react";
+import { fetchTrending } from "../../api";
+
 export default function HomePage() {
+  const [trending, setTrending] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const fetchedData = await fetchTrending();
+        setTrending(fetchedData);
+      } catch {
+        console.log(error);
+      }
+    }
+    fetchData();
+  }, []);
+
   return (
     <main>
-      <h1>Home page</h1>
+      <h1>Trending this week</h1>
+      <ul>
+        {trending.map((item) => {
+          return (
+            <li key={item.id}>
+              <a href="#">{item.original_title}</a>
+            </li>
+          );
+        })}
+      </ul>
     </main>
   );
 }
