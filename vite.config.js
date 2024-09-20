feaf8d4ed2a4becfbd6127b134200d0180a7bff6
culplate <culplate@gmail.com>
2024-09-20 16:32:04 +0200

    fix: switched to sass 'modern-compiler'

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,25 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import { globalStylesOptions } from "./global.styles";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { globalStylesOptions } from './global.styles';
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      modules: "/src/modules",
-      hooks: "/src/hooks",
-      pages: "/src/pages",
-      "@redux": "/src/redux",
-      shared: "/src/shared",
-      helpers: "/src/helpers",
-      context: "/src/context",
-      services: "/src/services",
+      modules: '/src/modules',
+      hooks: '/src/hooks',
+      pages: '/src/pages',
+      '@redux': '/src/redux',
+      shared: '/src/shared',
+      helpers: '/src/helpers',
+      context: '/src/context',
+      services: '/src/services',
     },
   },
   css: {
     preprocessorOptions: {
       scss: {
+        api: 'modern-compiler',
         additionalData: globalStylesOptions,
       },
     },
