56c71f0cb0b713a48f0df95d11fbe912b6a3a163
culplate <culplate@gmail.com>
2024-10-17 21:18:59 +0200

    upd: styles fix, added swiper llib
    fixed header z-index

--- src/services/themoviedbApi.js
+++ src/services/themoviedbApi.js
@@ -1,39 +1,64 @@
-import axios from 'axios'
+import axios from 'axios';
 
+//MOVE EVERYTHING TO THE BACKEND
 const URL = 'https://api.themoviedb.org/3/';
-const API_KEY = 'a5e5694b5c6ed25f9fc27a46f040f3b8';
-const API_TOKEN = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhNWU1Njk0YjVjNmVkMjVmOWZjMjdhNDZmMDQwZjNiOCIsInN1YiI6IjY1ZDEwYmNhNjY3NTFkMDE2MzMwZjBlYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.c8h12A5dXK4Ny8FGc_DKQnprEYQ-Rn819Y9Zg3Ffk_Y';
+const API_TOKEN =
+  'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhNWU1Njk0YjVjNmVkMjVmOWZjMjdhNDZmMDQwZjNiOCIsIm5iZiI6MTcyODg0ODc0Ny44NzEwOTgsInN1YiI6IjY1ZDEwYmNhNjY3NTFkMDE2MzMwZjBlYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.-UMs_ZdguBgOkYYrhZgKl3oh_nLPQ0enUGrZ7DH-dGQ';
 const language = 'language=en-US';
 
 const trendingParam = `trending/movie/week?include_adult=true&${language}&page=1`;
-const movieCredits = `movie/movie_id/credits?${language}`
+const movieCredits = `movie/movie_id/credits?${language}`;
 
 const options = {
-    headers: {
-        Authorization: `Bearer ${API_TOKEN}`
-    }
-}
+  headers: {
+    Authorization: `Bearer ${API_TOKEN}`,
+  },
+};
 export const fetchTrending = async () => {
-    const response = await axios.get(`${URL + trendingParam}`, options);
-    return response.data.results;
-}
+  const response = await axios.get(`${URL + trendingParam}`, options);
+  return response.data.results;
+};
 
 export const fetchByID = async (id) => {
-    const response = await axios.get(`${URL}movie/${id}`, options);
-    return response.data;
-}
+  const response = await axios.get(`${URL}movie/${id}`, options);
+  return response.data;
+};
 
 export const fetchCreditsByID = async (id) => {
-    const response = await axios.get(`${URL}movie/${id}/credits?${language}`, options);
-    return response.data.cast.slice(1, 11);
-}
+  const response = await axios.get(
+    `${URL}movie/${id}/credits?${language}`,
+    options
+  );
+  return response.data.cast.slice(1, 11);
+};
 
 export const fetchReviewsByID = async (id) => {
-    const response = await axios.get(`${URL}movie/${id}/reviews?${language}`, options);
-    return response.data.results.slice(1, 6);
-}
+  const response = await axios.get(
+    `${URL}movie/${id}/reviews?${language}`,
+    options
+  );
+  return response.data.results.slice(1, 6);
+};
 
 export const fetchByQuery = async (query) => {
-    const response = await axios.get(`${URL}search/movie?query=${query}&include_adult=true&${language}&page=1`, options);
-    return response.data.results
-}
+  const response = await axios.get(
+    `${URL}search/movie?query=${query}&include_adult=true&${language}&page=1`,
+    options
+  );
+  return response.data.results;
+};
+
+export const fetchGenres = async () => {
+  const response = await axios.get(
+    'https://api.themoviedb.org/3/genre/movie/list?language=en',
+    options
+  );
+  return response.data.genres;
+};
+
+export const fetchByGenreId = async (id) => {
+  const response = await axios.get(
+    `https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=true&language=en-US&page=1&sort_by=popularity.desc&with_genres=${id}`
+  );
+  return response.data.results;
+};
